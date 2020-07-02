1c3ea0735febb60c40968e581fabf1d71e992242
briens <bricensimamora26@gmail.com>
2020-07-02 17:35:01 +0700

    Laravel KUY

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.template')
+@section('title', 'Home')
+@section('container')
+<div class="container">
+    <div class="row"></div>
+        <div class="col"></div>
+        <h1 class="mt-4">Hello, world!</h1>
+</div>
+@endSection()  
---
+++ resources/views/profil.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.template')
+@section('title', 'About')
+@section('container')
+<div class="container">
+    <div class="row"></div>
+        <div class="col"></div>
+        <h1 class="mt-4">WE ARE {{$nama}}</h1>
+</div>
+@endSection()  
